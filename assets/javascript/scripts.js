e12bba8d90ccebd8a89e54c7a4aa0e4b1bd963a0
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-02-06 09:23:21 -0300

    sessões finalizadas

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,1 +1,12 @@
-console.log("Olá, mundo");
+const closeMenu = document.getElementById('items-menu');
+const logo = document.getElementById('logo');
+
+closeMenu.addEventListener('click', () => {
+    const navMenu = document.getElementById('close-menu');
+    navMenu.checked = false;
+})
+
+logo.addEventListener('click', () => {
+    const navLogo = document.getElementById('close-menu');
+    navLogo.checked = false;
+})
